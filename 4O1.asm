aa97e6b6279f37d94564e4072f368556cbd3186d
Mehdi <44398843+mehdiFeghhi@users.noreply.github.com>
2020-01-29 10:43:51 +0330

    Update 4O1.asm

--- 4O1.asm
+++ 4O1.asm
@@ -1,1 +1,25 @@
 
+%INCLUDE "lib.h"
+global _start
+section .bss
+input resb 2
+section .data
+hello db "Do you want to quit (Y/N):",0
+output_char db "      ds                          ",0
+
+section .code
+_start:
+ 
+     mov eax,'N';
+     while_2:
+        cmp eax,'Y'
+        je end_while_2
+        puts hello
+        fgets input,2
+        mov eax,[input];
+        jmp while_2
+      end_while_2:       
+end:
+mov eax,1
+mov ebx,0
+int 0x80
